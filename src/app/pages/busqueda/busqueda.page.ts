ba270f9763bf0d59bf47016a84e0557863c4bded
Eduardo <eduardmt@hotmail.com>
2021-05-12 12:24:03 -0500

    Pagina buscar

--- src/app/pages/busqueda/busqueda.page.ts
+++ src/app/pages/busqueda/busqueda.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.page.html',
@@ -6,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loadingControlloer: LoadingController,
+    private servicio: DataService
+  ) { }
 
   ngOnInit() {
+  }
+
+  buscar: string;
+  onSearchChange(event){
+    this.buscar = event.detail.value;
+    if(this.buscar == ''){
+      this.buscar == "";
+      console.log('no buscar'); 
+    }else {
+      setTimeout(()=>{
+        this.buscarPropiedadLoading();
+      }, 500);
+    }    
+  }
+  
+  responseData: any;
+  async buscarPropiedadLoading() {
+    const loading = await this.loadingControlloer.create({
+      message: 'Espere un momento',
+      mode: 'ios',
+      spinner: 'bubbles',
+      duration: 1500
+    });
+    await loading.present();
+    setTimeout(() => {
+      this.servicio.buscarPropiedad(this.buscar).subscribe(data => {
+        this.responseData = data;
+      })
+    },1600)
   }
  
 }
